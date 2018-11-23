8b96ab8d1a07dc90b6370cddb13abcc2a365bafd
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-11-22 19:55:05 -0500

    Completed

--- vampire.js
+++ vampire.js
@@ -5,27 +5,35 @@ class Vampire {
     this.offspring = [];
     this.creator = null;
   }
-
   /** Simple tree methods **/
-
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let test = this;
+    let count = 0;
+    while (test.name != 'root') {
+      test = test.creator;
+      count++;
+    }
+    return count;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if (this.numberOfVampiresFromOriginal <= vampire.numberOfVampiresFromOriginal) {
+      return true;
+    }
+    return false;
   }
 
   /** Stretch **/
@@ -42,3 +50,7 @@ class Vampire {
 
 module.exports = Vampire;
 
+//let rootVampire = new Vampire("root");
+//let offspring1 = new Vampire();
+//rootVampire.addOffspring(offspring1);
+//rootVampire.isMoreSeniorThan(offspring1)
